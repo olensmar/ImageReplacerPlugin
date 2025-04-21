c1d452a13e11deab50285bb47ebfb6837d35d6a2
Ole Lensmar <ole@lensmar.com>
2025-04-21 11:05:07 +0200

    updated for latest build of IntelliJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.olensmar"
-version = "0.1.1"
+version = "0.1.2"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232.1")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
     }
 
     signPlugin {
